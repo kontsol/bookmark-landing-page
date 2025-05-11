803d456a1f91926d0761fea44ef5c45eb54fa424
kontsol <kostastsolakidhs20@gmail.com>
2025-05-11 12:06:25 +0300

    commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button = ({ variant, text }) => {
+const Button = ({ variant, text, className }) => {
   const baseStyles =
     "px-4 lg:px-8 py-3 rounded-sm cursor-pointer shadow-[0px_4px_4px_rgba(0,0,0,0.25)] border-2 border-transparent  ";
 
@@ -13,7 +13,9 @@ const Button = ({ variant, text }) => {
     menu: "bg-transparent hover:bg-[#f7f7f7] w-full text-white hover:text-[#fa5757] border-white text-center py-4 heading-sm-500 mt-10 hover:border-[#fa5757] ",
   };
   return (
-    <div className={`${variantStyles[variant]} ${baseStyles}`}>{text}</div>
+    <div className={`${variantStyles[variant]} ${baseStyles} ${className}`}>
+      {text}
+    </div>
   );
 };
 
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import Button from "./Button";
 
 const Newsletter = () => {
   return (
-    <div className="block-gap paddingY bg-[#5368df] flex flex-col items-center justify-center content-gap">
-      <h1 className="text-[#f7f7f7] heading-sm-500 tracking-widest">
+    <div className="block-gap paddingY section-wrapper bg-[#5368df] flex flex-col items-center justify-center content-gap">
+      <h1 className="text-[#f7f7f7] heading-sm-500 tracking-[0.5rem]">
         35,000+ ALREADY JOINED
       </h1>
       <p className="textMaxWidth text-[#f7f7f7] text-center heading-md-500">
         Stay up-to-date with what we’re doing
       </p>
+      <form className="flex flex-col md:flex-row justify-between content-gap w-full max-w-lg border-2 border-lime-400">
+        <input
+          className="bg-[#f7f7f7] rounded-sm flex-grow px-4 py-3"
+          type="email"
+          placeholder="Enter your email address"
+        />
+
+        <Button variant="danger" text="Contact Us" className="" />
+      </form>
     </div>
   );
 };
